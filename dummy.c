7e15c45b2534eaf7218c1bfe314f764beda4c960
jzjerry <jzjerry@foxmail.com>
2026-03-20 16:23:52 +0800

    soc support update

--- dummy.c
+++ dummy.c
@@ -17,7 +17,23 @@ int main(){
     Tensor2D_Q8 x, w;
     printf("Hello MiCo!\n");
 
-    x.data = (int8_t*)malloc(N*K*sizeof(int8_t));
+    #ifdef SPRAM
+    uint32_t scratch_size = (uint32_t)__onchip_data_end-(uint32_t)__onchip_data_start;
+    printf("Scratch Pad RAM detected! Start: %x, End: %x, Size: %d\n",
+        (uint32_t)__onchip_data_start, 
+        (uint32_t)__onchip_data_end,
+        scratch_size
+    );
+    #endif
+
+    #ifdef SPRAM    
+    if(N*M*sizeof(int8_t) > scratch_size)
+        x.data = (int8_t*)scratch_malloc(N*K*sizeof(int8_t), 8);
+    else
+        x.data = (int8_t*)malloc(N*K*sizeof(int8_t));
+    #else
+        x.data = (int8_t*)malloc(N*K*sizeof(int8_t));
+    #endif
     w.data = (int8_t*)malloc(K*M*sizeof(int8_t));
 
     x.shape[0] = N;
